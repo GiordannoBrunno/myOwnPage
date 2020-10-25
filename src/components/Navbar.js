e5ed73228fef5eec6803a62abd2242b7d0a3370d
GiordannoBrunno <jrdnfrnndz@gmail.com>
2020-10-25 16:44:30 +0100

    Playing with backgrounds, tipos, and css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Navbar.css';
 
-import img from '/Users/jordan/Projects/myownpage-v1/src/img/chillsW.png';
+import img from '/Users/jordan/Projects/myownpage-v1/src/img/isotipoekkoW.png';
 
 function Navbar() {
     return(
-        <div>
+        <div className="navbar-container">
             <img className="logo" src={img} alt=""/>
+            <i><a className="contact-link" href="#">Contact</a></i>
         </div>
     )
 
--- src/components/PageSlider.js
+++ src/components/PageSlider.js
@@ -1,49 +1,18 @@
 import React from 'react';
 import './PageSlider.css';
-import video from "/Users/jordan/Projects/myownpage-v1/src/mediafiles/video3.mp4";
+import img from "/Users/jordan/Projects/myownpage-v1/src/img/bkg2.jpg";
+import img2 from "/Users/jordan/Projects/myownpage-v1/src/img/logotipoekkoW.png";
 
 function PageSlider() {
     return(
     <section className="banner">
         <div className="imgBox">
-            <video poster="" autoplay muted loop src={video} type="video/mp4" alt=""/>
+          <img src={img} alt=""/>
         </div>
 
-        <div className="contentBox">
-            <div>
-                <h2>Slide text one</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit viverra, dictum tincidunt natoque. Mauris in habitant class facilisis bibendum placerat sed justo lectus euismod ad, primis varius viverra volutpat aliquam vel aenean augue mattis lobortis eleifend quam, hac purus tortor mus proin porta sodales montes inceptos ac. Commodo id tortor curae parturient scelerisque vestibulum hac ut, laoreet tincidunt hendrerit auctor cursus euismod torquent habitasse, massa tristique arcu fermentum dui ligula vulputate.</p>
-                <a href="#">Details</a>
-            </div>
-            <div>
-                <h2>Slide text two</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit viverra, dictum tincidunt natoque. Mauris in habitant class facilisis bibendum placerat sed justo lectus euismod ad, primis varius viverra volutpat aliquam vel aenean augue mattis lobortis eleifend quam, hac purus tortor mus proin porta sodales montes inceptos ac. Commodo id tortor curae parturient scelerisque vestibulum hac ut, laoreet tincidunt hendrerit auctor cursus euismod torquent habitasse, massa tristique arcu fermentum dui ligula vulputate.</p>
-                <a href="#">Details</a>
-            </div>
-            <div>
-                <h2>Slide text three</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit viverra, dictum tincidunt natoque. Mauris in habitant class facilisis bibendum placerat sed justo lectus euismod ad, primis varius viverra volutpat aliquam vel aenean augue mattis lobortis eleifend quam, hac purus tortor mus proin porta sodales montes inceptos ac. Commodo id tortor curae parturient scelerisque vestibulum hac ut, laoreet tincidunt hendrerit auctor cursus euismod torquent habitasse, massa tristique arcu fermentum dui ligula vulputate.</p>
-                <a href="#">Details</a>
-            </div>
-            <div>
-                <h2>Slide text four</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit viverra, dictum tincidunt natoque. Mauris in habitant class facilisis bibendum placerat sed justo lectus euismod ad, primis varius viverra volutpat aliquam vel aenean augue mattis lobortis eleifend quam, hac purus tortor mus proin porta sodales montes inceptos ac. Commodo id tortor curae parturient scelerisque vestibulum hac ut, laoreet tincidunt hendrerit auctor cursus euismod torquent habitasse, massa tristique arcu fermentum dui ligula vulputate.</p>
-                <a href="#">Details</a>
-            </div>
+        <div className="logoBox">
+            <img className="center" src={img2} alt=""/>
         </div>
-
-        <ul className="controlU">
-            <li ></li>
-        </ul>
-        <ul className="controlR">
-            <li ></li>
-        </ul>
-        <ul className="controlD">
-            <li ></li>
-        </ul>
-        <ul className="controlL">
-            <li ></li>
-        </ul>
     </ section>
     );
  }
